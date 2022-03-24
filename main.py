c4ad72bae91b567261e935c2c5c3c15f2760f384
Chinazo Onwukaike <chinazoonwukaike@gmail.com>
2022-03-24 12:54:47 -0500

    Added example_func function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pytest
 def always_returns_true():
     return True
 
+def example_func():
+    return "This is an example function."
 
 def test_always_returns_true():
     assert always_returns_true()
