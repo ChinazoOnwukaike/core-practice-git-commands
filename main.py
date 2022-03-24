cfbf20f05c64f59457fb14649441a73313843ff1
Chinazo Onwukaike <chinazoonwukaike@gmail.com>
2022-03-24 13:23:49 -0500

    change none to true

--- main.py
+++ main.py
@@ -2,11 +2,7 @@ import pytest
 
 
 def always_returns_true():
-<<<<<<< HEAD
-    return None
-=======
     return True
->>>>>>> 041961a750efac700c991b6ff58a823151a4bfd2
 
 def example_func():
     return "This is an example function. Poppy make change to this function again "
